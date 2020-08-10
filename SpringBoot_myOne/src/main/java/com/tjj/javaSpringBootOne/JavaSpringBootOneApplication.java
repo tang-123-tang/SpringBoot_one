9f8e00fe078291b0b1155482e35a9ecdc37fb478
tang-123-tang <673424834@qq.com>
2020-08-10 13:52:29 +0800

    这是我写的第一个SpringBoot

---
+++ SpringBoot_myOne/src/main/java/com/tjj/javaSpringBootOne/JavaSpringBootOneApplication.java
@@ -0,0 +1,13 @@
+package com.tjj.javaSpringBootOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootOneApplication.class, args);
+    }
+
+}
---
+++ SpringBoot_myOne/src/test/java/com/tjj/javaSpringBootOne/JavaSpringBootOneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tjj.javaSpringBootOne;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaSpringBootOneApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
