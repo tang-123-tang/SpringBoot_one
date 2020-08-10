24afb27c7533a1627083e9df7fcefc82f20c1cfc
tang-123-tang <673424834@qq.com>
2020-08-10 18:54:56 +0800

    2020.8.10_这是我写的第一个SpringBoot

--- SpringBoot_myOne/src/main/java/com/tjj/javaSpringBootOne/test/controller/TextController.java
+++ SpringBoot_myOne/src/main/java/com/tjj/javaSpringBootOne/test/controller/TextController.java
@@ -1,5 +1,10 @@
 package com.tjj.javaSpringBootOne.test.controller;
 
+import com.tjj.javaSpringBootOne.test.vo.ApplicationText;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,13 +14,59 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/test")
 public class TextController {
+private final static Logger LOGGER=LoggerFactory.getLogger(TextController.class);
+    @Value("${server.port}")
+    private int port;
+    @Value("${com.age}")
+    private int age;
+    @Value("${com.desc}")
+    private String desc;
+    @Value("${com.name}")
+    private String name;
+    @Value("${com.random}")
+    private String random;
+@Autowired
+    ApplicationText applicationText;
+    /**
+     * 127.0.0.1:8085/test/testConfig
+     *
+     * @return
+     */
+    @GetMapping("/testConfig")
+    @ResponseBody
+    public String configTest() {
+
+        return new StringBuffer().append(port).append("------")
+                .append(age).append("------")
+                .append(name).append("------")
+                .append(desc).append("------")
+                .append(random).append("------").append("<br>")
+                .append(applicationText.getPort()).append("------")
+                .append(applicationText.getName()).append("------")
+                .append(applicationText.getDesc()).append("------")
+                .append(applicationText.getRandom()).append("------").toString();
+
+    }
+
     /**
      * 127.0.0.1:8080/test/test/testDesc
+     *
      * @return
      */
     @GetMapping("/testDesc")
     @ResponseBody
-    public String testDesc(){
+    public String testDesc() {
         return "that was my testDesc";
     }
+   @GetMapping("/testLog")
+    @ResponseBody
+    public String testLog(){
+       LOGGER.trace("that was a  trace log");
+       LOGGER.debug("that was a  debug log");
+       LOGGER.info("that was a  info log");
+       LOGGER.warn("that was a  warn log");
+       LOGGER.error("that was a  error log");
+               return "that was a testLog";
+    }
+
 }
---
+++ SpringBoot_myOne/src/main/java/com/tjj/javaSpringBootOne/test/vo/ApplicationText.java
@@ -0,0 +1,67 @@
+package com.tjj.javaSpringBootOne.test.vo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+@Component
+ @PropertySource("classpath:application.properties")
+ @ConfigurationProperties(prefix = "com.qq")
+public class ApplicationText {
+    private int port;
+    private String name;
+    private int age;
+    private String desc;
+    private String   random;
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getRandom() {
+        return random;
+    }
+
+    public void setRandom(String random) {
+        this.random = random;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public String toString() {
+        return "ApplicationText{" +
+                "port=" + port +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", desc='" + desc + '\'' +
+                ", random='" + random + '\'' +
+                '}';
+    }
+
+}
