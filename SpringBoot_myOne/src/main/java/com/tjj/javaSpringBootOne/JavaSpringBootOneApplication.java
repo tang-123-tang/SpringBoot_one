033a64e23e2ded6bf863406e10d705bf47de5be6
tang-123-tang <673424834@qq.com>
2020-08-10 13:58:24 +0800

    这是我写的第一个SpringBoot

--- SpringBoot_myOne/src/main/java/com/tjj/javaSpringBootOne/JavaSpringBootOneApplication.java
+++ SpringBoot_myOne/src/main/java/com/tjj/javaSpringBootOne/JavaSpringBootOneApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaSpringBootOneApplication {
-//
+
     public static void main(String[] args) {
         SpringApplication.run(JavaSpringBootOneApplication.class, args);
     }
